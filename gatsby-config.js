b51580381172a11ab137c8db016a89ca8a9541d8
lewiskwlaw <lewislawka@gmail.com>
2022-03-17 23:37:49 +0800

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
     siteMetadata: {
       title: `LK3B`,
       siteUrl: `https://lk3b.com`,
-      description: `Lewis & Phooebe's Blog`,
+      description: `Lewis & Phooebe`,
     },
   }
   
